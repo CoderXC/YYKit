bd08e5f0eb9f212c911faebb4efaee8f61341fa8
CoderXC <xiaobing_mailbox@163.com>
2016-06-23 19:55:24 +0800

    加入UILable的工厂创建方法

---
+++ YYKit/Base/UIKit/UILable+YYAdd.h
@@ -0,0 +1,13 @@
+//
+//  UILable+YYAdd.h
+//  YYKitDemo
+//
+//  Created by 小蔡 on 16/6/23.
+//  Copyright © 2016年 ibireme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILable_YYAdd : UILable
+
+@end
---
+++ YYKit/Base/UIKit/UILable+YYAdd.m
@@ -0,0 +1,13 @@
+//
+//  UILable+YYAdd.m
+//  YYKitDemo
+//
+//  Created by 小蔡 on 16/6/23.
+//  Copyright © 2016年 ibireme. All rights reserved.
+//
+
+#import "UILable+YYAdd.h"
+
+@implementation UILable_YYAdd
+
+@end
